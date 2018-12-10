0ec35bdd25d7f4db2af1278f6b59c3150366789d
nlakshma <nagarajan_lakshmanan@mcafee.com>
2018-12-10 18:45:13 +0530

    A Strongly-typed View

--- BethanysPieShop/Controllers/HomeController.cs
+++ BethanysPieShop/Controllers/HomeController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BethanysPieShop.Models;
+using BethanysPieShop.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -10,11 +12,23 @@ namespace BethanysPieShop.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IPieRepository _pieRepository;
+        public HomeController(IPieRepository pieRepository)
+        {
+            _pieRepository = pieRepository;
+        }
         // GET: /<controller>/
         public IActionResult Index()
         {
-            ViewBag.Message = "Welcome to Bethany’s Pie Shop";
-            return View();
+            var pies = _pieRepository.GetAllPies().OrderBy(p => p.Name);
+
+            var homeViewModel = new HomeViewModel()
+            {
+                Pies = pies.ToList(),
+                Title = "Welcome to Bethany's Pie Shop"
+            };
+
+            return View(homeViewModel);
         }
     }
 }
