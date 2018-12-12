7254517a77c65712f20e748c6a7eabbf3cdc3254
nlakshma <nagarajan_lakshmanan@mcafee.com>
2018-12-12 19:27:26 +0530

    Creating a Feedback Form

--- BethanysPieShop/Startup.cs
+++ BethanysPieShop/Startup.cs
@@ -30,6 +30,8 @@ namespace BethanysPieShop
 
             //services.AddTransient<IPieRepository, MockPieRepository>();
             services.AddTransient<IPieRepository, PieRepository>();
+            services.AddTransient<IFeedbackRepository, FeedbackRepository>();
+
             services.AddMvc();
         }
 
